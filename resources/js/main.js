a23d5fbe72b7ae0ea4c1514b9a26102b9bf7c21a
Shirshen Dada <72239897+SDG1060@users.noreply.github.com>
2020-12-31 21:43:00 +0600

    Update main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,3 +4,10 @@ $(".fireWorks").fireworks({
     width: "100%",
     height: "100%",
 });
+
+let parallax = document.querySelector(".parallax");
+window.addEventListener("mousemove", (e) => {
+    let x = e.x / 20;
+    let y = e.y / 20;
+    parallax.style.transform = `translate(${x}px, ${y}px)`;
+});
